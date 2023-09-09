7b7e1aa44dfb22c1732c549ef3952cdc779694d6
darkos11 <gouga.11@live.com>
2023-09-09 17:30:05 +0100

    fisrt commit

---
+++ src/Components/MovieCard.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./MovieCard.css";
+
+
+const MovieCard = ({title, description, rate, imageUrl, date}) => {
+  return (
+    <div>
+      <div className="card">
+        <a href="#">
+          <img className="img1" src={imageUrl}></img>
+          
+          <div className="title">{title}</div>
+          <div className="text">{description}</div>
+          <a href="#">
+            <div className="catagory">
+              Series <i className="fas fa-film"></i>
+            </div>
+          </a>
+          <a href="#">
+            <div className="views">
+              {date} <i className="far fa-eye"></i>{" "}
+            </div>
+          </a>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default MovieCard;
